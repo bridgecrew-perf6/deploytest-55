4cc467e1ed2d9345bb5666384cd48ac88da3adb1
Gordon Cheng <gordoncheng@outlook.com>
2022-01-07 20:35:45 +0800

    add index.js

---
+++ components/Hero.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Hero.module.css'
+export default function Hero() {
+
+  return (
+    <div className={styles.hero}>
+      <h1>Welcome</h1>
+      <h2>Energy</h2>
+    </div>
+  )
+}
