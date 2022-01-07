e6390d112ae39ca800d37d3c6a22d4c84061b3c4
Gordon Cheng <gordoncheng@outlook.com>
2022-01-07 20:22:57 +0800

    add build-client.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import buildClient from '../api/build-client'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentUser, footerData }) => {
+  return (
+    <Component currentUser={currentUser} {...pageProps} />
+  )
 }
 
-export default MyApp
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx, client, data.currentUser);
+  }
+
+  return {
+    pageProps,
+    ...data
+  }
+}
+
+export default AppComponent
